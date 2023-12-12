af01983e27f5f82da66128fbf79b8d9aa4bc59dd
AAsis-Shukla <169ashishkumar@gmail.com>
2023-12-12 12:44:35 +0530

    multiple file can be add using *

---
+++ python/forth.py
@@ -0,0 +1,1 @@
+print("this is my forth program in python")
---
+++ python/third.py
@@ -0,0 +1,7 @@
+# we can define any string by three type
+a = 'this is the first string' 
+b = "this is the second method of defining string"
+c = '''this is the third method of declaring string'''
+print(a)
+print(b)
+print(c)
